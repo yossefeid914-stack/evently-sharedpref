ff2ef0f071bcbcf7c51d74360e4ba6f67f2f883b
yossef <yossefeid914@gmail.com>
2026-02-07 22:15:56 +0200

    assets added

--- lib/screens/onboarding_screen/onboarding_screen.dart
+++ lib/screens/onboarding_screen/onboarding_screen.dart
@@ -6,6 +6,10 @@ class OnboardingScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Image.asset('assets/images/EvenlyLogo.png'),
+      ),
+    );
   }
 }
